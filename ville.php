d090b60c72ad4c25f95e005de95f5a7ffb6da343
hananeakhouzam <59393585+hananeakhouzam@users.noreply.github.com>
2020-06-02 18:36:04 +0100

    wip

--- ville.php
+++ ville.php
@@ -73,21 +73,18 @@ include'sidebar.html';
             <!--================================-->
             <div class="page-inner pd-0-force">
                <!-- File Manages -->
-           .<div class="col-md-6">
-              
+           .<div class="col-md-12">
+              <div class="d-flex justify-content-between align-items-center">
+                                 <div>
+                                    <h6 class="card-header-title tx-13 mb-0"> المدن والموزعين المكلفين بهم</h6>
+                                 </div>
+                              </div>
 <div class="row clearfix">
                      <!--================================-->
                      <!-- Basic dataTable Start -->
                      <!--================================-->
-                     <div class="col-md-12 col-lg-12">
+                     <div class="col-md-6">
                         <div class="card mg-b-30">
-                           <div class="card-header">
-                              <div class="d-flex justify-content-between align-items-center">
-                                 <div>
-                                    <h6 class="card-header-title tx-13 mb-0">  الطلبات عند الموزعين</h6>
-                                 </div>
-                              </div>
-                           </div>
                            <div class="card-body pd-0">
                               <table id="basicDataTable" class="table responsive nowrap">
                                  <thead>
